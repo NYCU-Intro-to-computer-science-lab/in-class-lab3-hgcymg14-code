b32fed364ded4616bf744c535b1fa35f4a41769b
hgcymg14-code <hg.c.y.mg14@nycu.edu.tw>
2025-11-25 13:58:27 +0800

    Add factorial and sum functions in p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -2,9 +2,17 @@
 
 #include <iostream>
 using namespace std;
-
+long long factorial(int n) {
+    if (n == 0 || n == 1) return 1;
+    return n * factorial(n - 1);
+}
+long long sum(int n) {
+    return n * (n + 1) / 2;
+}
 int main() {
-    
-    
+    int n;
+    cin >> n;
+    cout<<factorial(n)<<endl;
+    cout<<sum(n)<<endl;
     return 0;
 }
