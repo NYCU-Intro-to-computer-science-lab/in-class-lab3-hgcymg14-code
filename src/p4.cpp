e7739b73aa45d920b71e0fc757003d462ab7b543
hgcymg14-code <hg.c.y.mg14@nycu.edu.tw>
2025-11-25 14:00:18 +0800

    Implement Fibonacci function with tracing
    Added recursive Fibonacci function with debug output.

--- src/p4.cpp
+++ src/p4.cpp
@@ -3,8 +3,38 @@
 #include <iostream>
 using namespace std;
 
+void tab(int d) {
+    if (d == 0) return;
+    cout << "|--";
+    tab(d - 1);
+}
+
+int f(int n, int d) {
+    tab(d);
+    cout << "SEARCH fib(" << n << ")\n";
+
+    if (n == 1 || n == 2) {
+        tab(d);
+        cout << "GET fib(" << n << ") = 1\n";
+        return 1;
+    }
+
+    int x = f(n - 1, d + 1);
+    int y = f(n - 2, d + 1);
+    int z = x + y;
+
+    tab(d);
+    cout << "GET fib(" << n << ") = " << z << "\n";
+
+    return z;
+}
+
 int main() {
-    
-    
+    int n;
+    cin >> n;
+
+    int ans = f(n, 0);
+    cout << ans << "\n";
+
     return 0;
 }
