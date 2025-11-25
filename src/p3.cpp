821164eeb3b9d04daf1ceb34b3c49d35a9fa8812
hgcymg14-code <hg.c.y.mg14@nycu.edu.tw>
2025-11-25 15:05:57 +0800

    Refactor isPrime function and main logic formatting

--- src/p3.cpp
+++ src/p3.cpp
@@ -3,22 +3,22 @@
 #include <iostream>
 using namespace std;
 
-bool isPrime(int n) {
-    if (n < 2) return false;
-    for (int i = 2; i * i <= n; i++) { 
-        if (n % i == 0) return false;
+bool isPrime(int n){
+    if(n<2)return false;
+    for(int i=2;i*i<= n;i++){ 
+        if(n%i==0)return false;
     }
     return true;
 }
 
-int main() {
+int main(){
     int n;
-    cin >> n;
+    cin>>n;
 
-    for (int a = 2; a <= n / 2; a++) {
-        int b = n - a;
-        if (isPrime(a) && isPrime(b)) {
-            cout << a << " " << b;
+    for(int a=2;a<=n/2;a++){
+        int b=n-a;
+        if(isPrime(a)&&isPrime(b)){
+            cout<<a<<" "<<b;
             return 0; 
         }
     }
