d6b25b93b2bc262dfe7f514eac6010356e4bef80
hgcymg14-code <hg.c.y.mg14@nycu.edu.tw>
2025-11-25 15:03:36 +0800

    Improve formatting in Fibonacci function
    Refactor formatting and spacing in the Fibonacci function.

--- src/p4.cpp
+++ src/p4.cpp
@@ -9,32 +9,32 @@ void tab(int d) {
     tab(d - 1);
 }
 
-int f(int n, int d) {
+int f(int n, int d){
     tab(d);
-    cout << "SEARCH fib(" << n << ")\n";
+    cout<<"SEARCH fib("<<n<<")\n";
 
-    if (n == 1 || n == 2) {
+    if (n==1||n==2){
         tab(d);
-        cout << "GET fib(" << n << ") = 1\n";
+        cout<<"GET fib("<<n<<")=1\n";
         return 1;
     }
 
-    int x = f(n - 1, d + 1);
-    int y = f(n - 2, d + 1);
-    int z = x + y;
+    int x=f(n-1,d+1);
+    int y=f(n-2,d+1);
+    int z=x+y;
 
     tab(d);
-    cout << "GET fib(" << n << ") = " << z << "\n";
+    cout<<"GET fib("<<n<<")="<<z<<"\n";
 
     return z;
 }
 
-int main() {
+int main(){
     int n;
-    cin >> n;
+    cin>>n;
 
-    int ans = f(n, 0);
-    cout << ans << "\n";
+    int ans=f(n,0);
+    cout<<ans<<"\n";
 
     return 0;
 }
