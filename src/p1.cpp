404152ec10fc32a3381cae404f2ae50097a0d267
hgcymg14-code <hg.c.y.mg14@nycu.edu.tw>
2025-11-25 15:09:47 +0800

    Improve formatting and readability in p1.cpp
    Refactor factorial and sum functions for readability.

--- src/p1.cpp
+++ src/p1.cpp
@@ -2,17 +2,17 @@
 
 #include <iostream>
 using namespace std;
-long long factorial(int n) {
-    if (n == 0 || n == 1) return 1;
-    else return n * factorial(n - 1);
+long long factorial(int n){
+    if(n==0||n==1)return 1;
+    else return n*factorial(n-1);
 }
-long long sum(int n) {
-    if (n == 0 || n == 1) return n;
+long long sum(int n){
+    if(n==0||n ==1)return n;
     else return n+sum(n-1);
 }
-int main() {
+int main(){
     int n;
-    cin >> n;
+    cin>>n;
     cout<<factorial(n)<<endl;
     cout<<sum(n)<<endl;
     return 0;
